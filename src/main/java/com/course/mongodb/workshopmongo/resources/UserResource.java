bfd3972a75cb3dbaa4a7210a11e196958b506ff5
victorpereira <dev.victorhugo@gmail.com>
2022-03-03 18:25:18 -0300

    Atualização de usuário com PUT

--- src/main/java/com/course/mongodb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/course/mongodb/workshopmongo/resources/UserResource.java
@@ -45,4 +45,12 @@ public class UserResource {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User user = service.fromDTO(objDto);
+        user.setId(id);
+        user = service.update(user);
+        return ResponseEntity.noContent().build();
+    }
 }
